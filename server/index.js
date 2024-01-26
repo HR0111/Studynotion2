7ef69be9c88d072aa40845bb10aa619441c22e7c
Hemant Rathod <hemantrathod0111@gmail.com>
2024-01-26 07:25:41 +0530

    yes

--- server/index.js
+++ server/index.js
@@ -25,6 +25,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(cors())
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+	res.header('Access-Control-Allow-Origin', '*');
+	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+	next();
+  });
+
 app.use(
 	cors({
 		// origin:"http://localhost:3000",
